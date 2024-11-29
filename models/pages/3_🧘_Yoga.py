0ca7de9f8d4a9c8da4dfd0cb8148058e2d0d2850
MADHAV KALRA <68096582+madhavkalra7@users.noreply.github.com>
2024-11-29 15:21:54 +0530

    Update 3_🧘_Yoga.py

--- models/pages/3_🧘_Yoga.py
+++ models/pages/3_🧘_Yoga.py
@@ -6,9 +6,9 @@ import streamlit as st
 import mediapipe as mp
 import numpy as np
 from PIL import Image
-from playsound import playsound
+# from playsound import playsound
 
-st.set_page_config(page_title="Yoga", page_icon="D:\PBL-3\FE.png", layout="wide")
+st.set_page_config(page_title="Yoga", page_icon="C:/Users/hp/Desktop/AI-Fitness-Trainer-main/AI-Fitness-Trainer-main/models/images/FE.png", layout="wide")
 
 css_styles = """
     <style>
@@ -90,13 +90,13 @@ last_second = 0
 counter = 0
 pose_number = 1
 
-img1 = Image.open(r"D:\Downloads\AI-Fitness-Trainer-main\AI-Fitness-Trainer-main\models\gif\yoga.gif")
-img2 = Image.open(r"D:\Downloads\AI-Fitness-Trainer-main\AI-Fitness-Trainer-main\models\images\pranamasana2.png")
-img3 = Image.open(r"D:\Downloads\AI-Fitness-Trainer-main\AI-Fitness-Trainer-main\models\images\Eka_Pada_Pranamasana.png")
-img4 = Image.open(r"D:\Downloads\AI-Fitness-Trainer-main\AI-Fitness-Trainer-main\models\images\Ashwa_Sanchalanasana.webp")
-img5 = Image.open(r"D:\Downloads\AI-Fitness-Trainer-main\AI-Fitness-Trainer-main\models\images\ardha_chakrasana.webp")
-img6 = Image.open(r"D:\Downloads\AI-Fitness-Trainer-main\AI-Fitness-Trainer-main\models\images\Utkatasana.png")
-img7 = Image.open(r"D:\Downloads\AI-Fitness-Trainer-main\AI-Fitness-Trainer-main\models\images\Veerabhadrasan_2.png")
+img1 = Image.open("models/gif/yoga.gif")
+img2 = Image.open("models/images/pranamasana2.png")
+img3 = Image.open("models/image/Eka_Pada_Pranamasana.png")
+img4 = Image.open("models/images/Ashwa_Sanchalanasana.webp")
+img5 = Image.open("models/images/ardha_chakrasana.webp")
+img6 = Image.open("models/images/Utkatasana.png")
+img7 = Image.open("models/images/Veerabhadrasan_2.png")
 
 
 mp_drawing = mp.solutions.drawing_utils
